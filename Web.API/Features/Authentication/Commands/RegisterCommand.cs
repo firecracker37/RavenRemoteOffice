5b2edb6b8b5c5b105917eb6a6a669acee429d56e
Tom Richardson <firecracker37@gmail.com>
2023-10-16 09:40:05 -0500

    Updated RegisterCommand to automatically assign new users to Roles.Employee

--- Web.API/Features/Authentication/Commands/RegisterCommand.cs
+++ Web.API/Features/Authentication/Commands/RegisterCommand.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Web.API.Constants;
+using Web.API.Features.Authentication.DTOs;
 using Web.API.Features.Authentication.Services;
 using Web.Shared.DTOs;
 
@@ -22,14 +24,48 @@ namespace Web.API.Features.Authentication.Commands
 
         public async Task<IdentityResult> ExecuteAsync(RegisterUserDTO model)
         {
-            var result = await _identityService.RegisterUserAsync(model);
+            // Register the user
+            var registrationResult = await _identityService.RegisterUserAsync(model);
 
-            if (result.Succeeded)
+            if (registrationResult.Succeeded)
             {
+                // Log if unable to send verification email
                 var emailResult = await _sendEmailVerificationEmailCommand.ExecuteAsync(model.Email);
-                if (!emailResult.Success) _logger.LogWarning($"Unable to send email verification to {model.Email}. Message: {emailResult.ErrorMessage}");
+                if (!emailResult.Success)
+                {
+                    _logger.LogWarning($"Unable to send email verification to {model.Email}. Message: {emailResult.ErrorMessage}");
+                }
+
+                // Get the newly registered user
+                var user = await _identityService.FindUserByEmailAsync(model.Email);
+
+                // Check if the user retrieval was successful and user is not null
+                if (user != null)
+                {
+                    // Create a UserWithRoleDTO object with the user and the Employee role
+                    var userWithRole = new UserWithRoleDTO
+                    {
+                        User = user,
+                        Role = Roles.Employee
+                    };
+
+                    // Add the user to the Employee role
+                    var roleAssignmentResult = await _identityService.AddUserToRoleAsync(userWithRole);
+
+                    // Optionally, log if unable to assign the role
+                    if (!roleAssignmentResult.Succeeded)
+                    {
+                        _logger.LogWarning($"Unable to assign Employee role to {model.Email}. Errors: {string.Join(", ", roleAssignmentResult.Errors.Select(e => e.Description))}");
+                    }
+                }
+                else
+                {
+                    _logger.LogWarning($"Unable to retrieve the user {model.Email} after registration.");
+                }
             }
-            return result;
+
+            return registrationResult;
         }
+
     }
 }
